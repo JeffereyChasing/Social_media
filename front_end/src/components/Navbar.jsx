e4a41f33316c8631ea09a5e8b6f221a6b3a843e1
YanqinWu <wu1149823510@gmail.com>
2022-10-25 15:05:44 +0800

    1

--- front_end/src/components/Navbar.jsx
+++ front_end/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ if(!user) return null;
     <div className = 'flex gap-2 md:gap-5 w-full mt-5 pb-7'>
 
         <div className = "flex justify-start items-center w-full outline-none focus-within:shadow-small px-2 rounded-md bg-white border-none">
-          <IoMdSearch fontsize = {21} className = 'ml-1'/>
+          <IoMdSearch fontSize = {21} className = 'ml-1'/>
           <input type = 'text' onChange = {(e) => setSearchTerm(e.target.value)} 
           placeholder = 'Search' value = {searchTerm} onFocus ={()=>navigate('/search')}
           className = 'p-2 w-full bg-white outline-none'/>
--- front_end/src/components/Pin.jsx
+++ front_end/src/components/Pin.jsx
@@ -6,15 +6,37 @@ import {MdDownload, MdDownloadForOffline} from 'react-icons/md'
 import {AiTwotoneDelete} from 'react-icons/ai'
 import {BsFillArrowUpRightCircleFill} from 'react-icons/bs'
 import {useState,useEffect} from 'react';
+import { fetchUser } from '../utils/fetchUser';
 
 
 
 
-const Pin = ({pin:{postedBy,image,_id,destination}}) => {
+const Pin = ({pin:{postedBy,image,_id,destination,save}}) => {
   
   const [postHovered, setPostHovered] = useState(false)
-  const [savingPost, setSavingPost] = useState(false)
   const navigate = useNavigate()
+  const user = fetchUser();
+  const alreadySaved = !!(save?.filter((item) => item.postedBy._id ===user.googleId))?.length;
+  /* check whether the user (by googleId) already saved the pin*/
+  /* !! turn alreadySaved into boolean values*/
+  /* ?.length to avoid error because a pin may not be saved at all (.save = null)*/
+
+
+  const savePin = (id) => {
+    if (!alreadySaved) {
+        client.patch(id).setIfMissing({save:[]}).insert("after",'save[-1]',[{
+          key:uuidv4(),
+          userId: user.googleId,
+          postedBy:{
+            _type:'postedBy',
+            _ref: user.googleId
+          }
+        }]).commit().then(()=>{
+          window.location.reload();
+        } );
+    }
+
+  }
 
   return (
     <div className = 'm-2'>
@@ -27,21 +49,51 @@ const Pin = ({pin:{postedBy,image,_id,destination}}) => {
           {postHovered && (
             <div className = 'absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50' style ={{height:'100%'}}>
               <div className = 'flex items-center justify-between'>
+
+
+                {/*Download button*/}
                 <div className = 'flex gap-2'>
                   <a href = {`${image?.asset?.url}?dl=`} download onClick = {(e)=>e.stopPropagation()}
-                  className ='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 shadow-md outline-none'>
 
+                  /*stopPropogation is to stop the extra navigation to pindetail after download*/
+
+                  className ='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 shadow-md outline-none'>
                   <MdDownloadForOffline/>
                   </a>
                 </div>
+
+
+                {/*Saved button*/}
+                {alreadySaved ? (
+                  <button type = "button" className = "bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-based rounded-3xl hover:sahdow-md outline-none">
+                    {save?.length} Saved
+                  </button>
+                ):(
+                  <button onClick = {(e)=>{ e.stopPropagation();savePin(_id);}}
+                  type = "button" className = "bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-based rounded-3xl hover:sahdow-md outline-none">
+                    Save
+                  </button>
+                )}
               </div>
+                
+                {/*destination button*/}
+              <div className = 'flex justify-between items-center gap-2 w-full'>
+                  {destination && (
+                    <a href = {destination} target = 'blank' rel = 'noreferrer' 
+                    className = 'bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md'>
+                        <BsFillArrowUpRightCircleFill/>
+                        {destination.length > 20 ? destination.slice(8,20): destination.slice(8) }
+                      </a>
+                  )}
+              </div>
+
+
+
+
+
             </div>
           )}
-
-
-      </div>
-
-      
+      </div>   
     </div>
   )
 }
